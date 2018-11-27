4f7621a1383ce1dab7ff4a4c4b5ccf835974ca72
gargdikshit <45335029+gargdikshit@users.noreply.github.com>
2018-11-27 14:25:49 +0530

    added git

--- Vagrant/3-tier/node.sh
+++ Vagrant/3-tier/node.sh
@@ -9,3 +9,12 @@ if [ "" == "$NODE_INSTALLED" ]; then
   curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
   apt-get install -y build-essential nodejs
 fi
+GIT="git"
+# Prerequisites
+GIT_INSTALLED=$(dpkg-query -W --showformat='${Status}\n' $GIT | grep "install ok installed")
+echo "Checking for $GIT: $GIT_INSTALLED"
+if [ "" == "$GIT_INSTALLED" ]; then
+  apt-get update
+  apt-get install -y $GIT
+fi
+
